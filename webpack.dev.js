fa34bfa94426b58b89a69148e06f0b6ca4f048c2
woong <ckehfqkr302@naver.com>
2024-09-04 23:12:24 +0900

    chore : webpack 설정

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+});
