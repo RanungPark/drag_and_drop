b321f07ce57e599b161f6b19b7f598265518b67d
woong <ckehfqkr302@naver.com>
2024-09-06 21:14:05 +0900

    feat : color theme 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  bg_dark: '#edede9',
+  bg: '#ffffff',
+  bg_light: '#fafafa',
+  primary_dark: '#d5bdaf',
+  primary: '#f5ebe0',
+  primary_light: '#e3d5ca',
+  netural_dark: '#212529',
+  netural: '#343a40',
+  netural_light: '#495057',
+  warning: '#c9184a',
+};
+
+export const theme = {
+  colors,
+};
+
+export type Theme = typeof theme;
