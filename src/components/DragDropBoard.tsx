a88f3397e4d35f9404db94c1a21d0ec1b926a8e7
woong <ckehfqkr302@naver.com>
2024-09-08 21:39:41 +0900

    docs : Responders에 커스텀 훅 적용

--- src/components/DragDropBoard.tsx
+++ src/components/DragDropBoard.tsx
@@ -1,21 +1,69 @@
-import React from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+import React, { useCallback, useState } from 'react';
+import { DragDropContext, DragStart, DragUpdate, DropResult } from 'react-beautiful-dnd';
 import DroppableColumn from './DroppableColumn';
 import styled from 'styled-components';
 import useDragDropContext from '@hooks/useDragDropContex';
 import AddDroppableColumn from './AddDroppableColumn';
-import useOnDragEnd from '@hooks/useOnDragEnd';
+import useIsSelectedEven from '@hooks/useIsSelectedEven';
+import useSelected from '@hooks/useSelected';
 
 const DragDropBoard = () => {
-  const { columns } = useDragDropContext();
+  const { columns, thirdColunmsKey, handleSameColumnReorder, handleDiffColumnReorder } =
+    useDragDropContext();
+  const { selectedItemId, selectedIndex, selectedColumnkey, handleSelected, resetSelected } =
+    useSelected();
+  const { isSelectedEven, handleSelectedEven, resetIsSelectedEven } = useIsSelectedEven();
 
-  const [onDragEnd] = useOnDragEnd();
+  const onDragStart = (start: DragStart) => {
+    handleSelected(start);
+  };
+
+  const onDragUpdate = (update: DragUpdate) => {
+    const { destination } = update;
+    if (!destination) return;
+
+    const selectColumnLength = columns[selectedColumnkey].length - 1;
+
+    handleSelectedEven(selectedIndex, selectedColumnkey, update, selectColumnLength);
+  };
+
+  const onDragEnd = useCallback(
+    (result: DropResult) => {
+      const { destination, source } = result;
+
+      resetSelected();
+
+      if (!destination) {
+        return;
+      }
+
+      const destinationColunmLength = columns[destination.droppableId].length - 1;
+
+      if (resetIsSelectedEven(result, destinationColunmLength)) return;
+
+      if (destination.droppableId === thirdColunmsKey) return;
+
+      if (source.droppableId === destination.droppableId) {
+        handleSameColumnReorder({ destination, source });
+      } else if (source.droppableId !== destination.droppableId) {
+        handleDiffColumnReorder({ destination, source });
+      }
+    },
+
+    [columns],
+  );
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
       <Board>
         {Object.keys(columns).map((key) => (
-          <DroppableColumn key={key} items={columns[key]} columnKey={key} />
+          <DroppableColumn
+            key={key}
+            items={columns[key]}
+            columnKey={key}
+            isSelectedEven={isSelectedEven}
+            selectedItemId={selectedItemId}
+          />
         ))}
 
         <AddDroppableColumn />
