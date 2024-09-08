b6397391ffc454e21853dbdc85f32d3003af3b98
woong <ckehfqkr302@naver.com>
2024-09-08 21:37:34 +0900

    feat : useSelected 생성

---
+++ src/hooks/useSelected.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { DragStart } from 'react-beautiful-dnd';
+
+const useSelected = () => {
+  const [selectedItemId, setSelectedItemId] = useState('');
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedColumnkey, setSelectedColumnkey] = useState('');
+
+  const handleSelected = ({ source, draggableId }: DragStart) => {
+    setSelectedItemId(draggableId);
+    setSelectedIndex(source.index);
+    setSelectedColumnkey(source.droppableId);
+  };
+  const resetSelected = () => {
+    setSelectedItemId('');
+    setSelectedIndex(-1);
+    setSelectedColumnkey('');
+  };
+
+  return {selectedItemId, selectedIndex, selectedColumnkey, handleSelected, resetSelected};
+};
+
+export default useSelected;
